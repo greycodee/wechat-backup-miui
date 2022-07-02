dbdee17503a5d8d1e0e5b82ac40cd0cb39e4a7cd
greycodee <zhengminghui99@gmail.com>
2022-07-02 17:19:25 +0800

    add flag

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"path/filepath"
 )
 
-var srcDir = "/Users/zheng/Documents/miuibak"
-var dstDir = "/Users/zheng/Documents/miuibak2"
+var srcDir = flag.String("s", "", "src dir")
+var dstDir = flag.String("d", "", "dst dir")
+
+func init() {
+	flag.Parse()
+	if *srcDir == "" || *dstDir == "" {
+		log.Fatalln("请输入src和dst目录")
+	}
+}
 
 func main() {
 
@@ -14,20 +22,20 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	Unzip(filepath.Join(srcDir, "backup_wechat.zip"), srcDir)
+	Unzip(filepath.Join(*srcDir, "backup_wechat.zip"), *srcDir)
 
 	// 去除MIUI头文件
-	CutMiuiBak(filepath.Join(srcDir, "wechat.bak"))
+	CutMiuiBak(filepath.Join(*srcDir, "wechat.bak"))
 
 	// 解包
-	dockerCli.RunWithDelete("greycodee/abe", []string{srcDir + ":/bak"},
+	dockerCli.RunWithDelete("greycodee/abe", []string{*srcDir + ":/bak"},
 		[]string{"java", "-jar", "abe.jar", "unpack", "/bak/wechat_miui.bak", "/bak/unpackGo.tar"})
 
-	err = UnPackTar(filepath.Join(srcDir, "unpackGo.tar"), srcDir)
+	err = UnPackTar(filepath.Join(*srcDir, "unpackGo.tar"), *srcDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	infoList := GetWechatAccountInfo(srcDir)
-	ArchiveFile(srcDir, dstDir, infoList, dockerCli)
+	infoList := GetWechatAccountInfo(*srcDir)
+	ArchiveFile(*srcDir, *dstDir, infoList, dockerCli)
 }
